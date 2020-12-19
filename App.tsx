d9b0dc052b08ab338bd2a855509ae044138a4e4e
Joseph Huang <joseph.huang@epita.fr>
2020-12-19 23:02:44 +0100

    create directory

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Screen from './Screen';
-import LoginScreen from './LoginScreen';
+import Screen from './screens/Screen';
+import LoginScreen from './screens/LoginScreen';
 import * as firebase from 'firebase';
 import Swiper from 'react-native-swiper';
 
