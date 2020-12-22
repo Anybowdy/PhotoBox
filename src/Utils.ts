e79c6bce8c7726425e6041c549130c08f7b58b97
Joseph Huang <joseph.huang@epita.fr>
2020-12-22 21:41:42 +0100

    added function to utils

--- src/Utils.ts
+++ src/Utils.ts
@@ -6,4 +6,26 @@ const uuidv4 = () => {
   });
 };
 
-export { uuidv4 };
+const getReadableFromTimestamp = (timestamp: number) => {
+  let now = new Date().getTime();
+  var result: number = -1;
+  var timeType: string = '';
+
+  let sec = Math.floor((now - timestamp) / 1000);
+  let min = Math.floor(sec / 60);
+  let hour = Math.floor(min / 60);
+
+  if (sec < 60) {
+    result = sec;
+    timeType = 'seconde';
+  } else if (min < 60) {
+    result = min;
+    timeType = 'minute';
+  } else if (hour < 24) {
+    result = hour;
+    timeType = 'heure';
+  }
+  return 'Il y a ' + result + ' ' + timeType + (result > 1 ? 's' : '');
+};
+
+export { uuidv4, getReadableFromTimestamp };
--- src/screens/ContentScreen.tsx
+++ src/screens/ContentScreen.tsx
@@ -1,34 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import useItems from '../hooks/useItems';
-import { AntDesign } from '@expo/vector-icons';
-import * as firebase from 'firebase';
 import ThumbImage from '../components/ThumbImage';
+
+import { getReadableFromTimestamp } from '../Utils';
 
 const ContentScreen = () => {
   const { items } = useItems();
-
-  const getReadableFromTimestamp = (timestamp: number) => {
-    let now = new Date().getTime();
-    var result: number = -1;
-    var timeType: string = '';
-
-    let sec = Math.floor((now - timestamp) / 1000);
-    let min = Math.floor(sec / 60);
-    let hour = Math.floor(min / 60);
-
-    if (sec < 60) {
-      result = sec;
-      timeType = 'seconde';
-    } else if (min < 60) {
-      result = min;
-      timeType = 'minute';
-    } else if (hour < 24) {
-      result = hour;
-      timeType = 'heure';
-    }
-    return 'Il y a ' + result + ' ' + timeType + (result > 1 ? 's' : '');
-  };
 
   return (
     <View
