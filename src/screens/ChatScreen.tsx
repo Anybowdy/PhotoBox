7d3c7f3a23ace65844f207a0b29a8438b63afb38
Joseph Huang <joseph.huang@epita.fr>
2020-12-24 20:59:06 +0100

    added text input

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,89 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 const ChatScreen = () => {
-  const items = [
+  const [items, setItems] = useState([
     {
-      id: '1',
+      message: {
+        id: '1',
+        author: 'Joseph',
+        body: 'Hello how are you',
+      },
     },
-    {
-      id: '12',
-    },
-    {
-      id: '13',
-    },
-    {
-      id: '14',
-    },
-    {
-      id: '141',
-    },
-    {
-      id: '142',
-    },
-    {
-      id: '143',
-    },
-    {
-      id: '144',
-    },
-    {
-      id: '145',
-    },
-    {
-      id: '140',
-    },
-    {
-      id: '1400',
-    },
-    {
-      id: '1408',
-    },
-    {
-      id: '14785',
-    },
-  ];
+  ]);
 
-  var MessageCell = () => (
+  var MessageCell = ({ item }: { item: any }) => (
     <View
       style={{
         width: '100%',
         backgroundColor: 'white',
-        padding: '3%',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
         justifyContent: 'space-evenly',
       }}
     >
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <Text
-          style={{ marginBottom: 10, marginRight: 10, fontWeight: '600', fontSize: 18 }}
+          style={{ marginBottom: 10, marginRight: 10, fontWeight: '600', fontSize: 17 }}
         >
-          Joseph
+          {item.message.author}
         </Text>
         <Text style={{ marginBottom: 10, color: 'rgba(0,0,0,0.5)' }}>
           Aujourd’hui à 14:34
         </Text>
       </View>
-      <Text style={{ fontSize: 18 }}>Hello, how are you</Text>
+      <KeyboardAvoidingView>
+        <Text style={{ fontSize: 16 }}>{item.message.body}</Text>
+      </KeyboardAvoidingView>
     </View>
   );
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'yellow',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior='padding'
+      keyboardVerticalOffset={-20}
     >
-      <FlatList
-        inverted={true}
-        style={{ backgroundColor: 'gray', width: '100%' }}
-        keyExtractor={(item) => item.id}
-        data={items}
-        renderItem={({ item, index }) => <MessageCell />}
-      />
-    </View>
+      <View
+        style={{
+          flex: 1,
+          //backgroundColor: 'yellow',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FlatList
+          inverted={true}
+          style={{ backgroundColor: 'gray', width: '100%' }}
+          keyExtractor={(item) => item.message.id}
+          data={items}
+          renderItem={({ item, index }) => <MessageCell item={item} />}
+        />
+        <View
+          style={{
+            width: '100%',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            height: 100,
+            paddingVertical: 10,
+          }}
+        >
+          <TextInput
+            placeholder='Ecrire un message...'
+            style={{
+              paddingHorizontal: 15,
+              width: '95%',
+              height: 50,
+              borderRadius: 30,
+              backgroundColor: 'rgba(0,0,0,0.1)',
+            }}
+          />
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
